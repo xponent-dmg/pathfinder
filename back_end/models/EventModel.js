ff1032b55738dc9c647de3811d8ee3fceadf4cc2
xponent-dmg <harishankar.r2102@gmail.com>
2025-03-27 19:12:34 +0530

    changed the event schema to add image url

--- back_end/models/EventModel.js
+++ back_end/models/EventModel.js
@@ -1,27 +1,39 @@
 //For the Events
-const mongoose = require('mongoose');
-const EventSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const EventSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
-    building: { type: mongoose.Schema.Types.ObjectId, ref: 'Building', required: true },
+    building: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Building",
+      required: true,
+    },
+    image: { type: String, required: true },
     startTime: { type: Date, required: true },
     endTime: { type: Date, required: true },
     information: { type: String },
-    roomno: { type: String, required: true},
+    roomno: { type: String },
     categories: [{ type: String }],
     clubName: { type: String, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'ClubLeader', required: true }
-  }, {
-    timestamps: true 
-  });
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ClubLeader",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-  // Ensure startTime is before endTime
-  EventSchema.pre('save', function(next) {
-    if (this.startTime >= this.endTime) {
-      return next(new Error('Start time must be before end time'));
-    }
-    next();
-  });
+// Ensure startTime is before endTime
+EventSchema.pre("save", function (next) {
+  if (this.startTime >= this.endTime) {
+    return next(new Error("Start time must be before end time"));
+  }
+  next();
+});
 
-  EventSchema.index({ categories: 1 }); //Categories in ascending order
-  EventSchema.index({ startTime: 1, endTime: 1 }); //Dates in ascending order
-  module.exports = mongoose.model('Event', EventSchema);
+EventSchema.index({ categories: 1 }); //Categories in ascending order
+EventSchema.index({ startTime: 1, endTime: 1 }); //Dates in ascending order
+module.exports = mongoose.model("Event", EventSchema);
