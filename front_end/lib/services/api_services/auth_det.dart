44d9ccd2ba577cf76438f99076022cb1a1431112
xponent-dmg <harishankar.r2102@gmail.com>
2025-07-12 09:48:45 +0530

    changed the server platform to render

--- front_end/lib/services/api_services/auth_det.dart
+++ front_end/lib/services/api_services/auth_det.dart
@@ -1,8 +1,5 @@
 class AuthDet {
-  // final String _baseUrl = "http://192.168.95.119:3000"; //vyshnav
-  // final String _baseUrl = "http://192.168.95.242:3000"; //Nandu
-  // final String _baseUrl = "http://192.168.90.165:3000"; //Dhruv
-  final String _baseUrl = "https://pathfinder-production-0493.up.railway.app";
+  final String _baseUrl = "https://pathfinder-server-2uk7.onrender.com";
   final String _supaBaseUrl = 'https://zttcqbheotutkxaqljvf.supabase.co';
   final String _supaBaseAnon =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0dGNxYmhlb3R1dGt4YXFsanZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNzQ4ODAsImV4cCI6MjA1ODY1MDg4MH0._LikwV4TS1IXI76Jsz0El8P6rsFwL6LiGohiEt9Wexw';
--- front_end/lib/services/api_services/maps_api.dart
+++ front_end/lib/services/api_services/maps_api.dart
@@ -2,5 +2,4 @@ import 'package:path_finder/services/api_services/auth_det.dart';
 
 class MapsApi {
   final String baseUrl = AuthDet().baseUrl;
-  // "https://pathfinder-production-aa04.up.railway.app"; //server on railway
 }
