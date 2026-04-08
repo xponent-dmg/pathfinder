487f81d82fe96936ea02e5db244ca82d25a45d97
xponent-dmg <harishankar.r2102@gmail.com>
2026-04-08 15:34:26 +0530

    changed the supabase details

--- front_end/lib/services/api_services/auth_det.dart
+++ front_end/lib/services/api_services/auth_det.dart
@@ -1,8 +1,8 @@
 class AuthDet {
   final String _baseUrl = "https://pathfinder-docker-anx7.onrender.com";
-  final String _supaBaseUrl = 'https://zttcqbheotutkxaqljvf.supabase.co';
+  final String _supaBaseUrl = 'https://yysnejxcxuddjxshfzde.supabase.co';
   final String _supaBaseAnon =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0dGNxYmhlb3R1dGt4YXFsanZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNzQ4ODAsImV4cCI6MjA1ODY1MDg4MH0._LikwV4TS1IXI76Jsz0El8P6rsFwL6LiGohiEt9Wexw';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5c25lanhjeHVkZGp4c2hmemRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MzU1OTYsImV4cCI6MjA5MTIxMTU5Nn0.iP26owIxMOWgvO-ygGBvN5g4KsGVw1_9kDoBogj8FnQ';
 
   String get baseUrl => _baseUrl;
   String get supaBaseUrl => _supaBaseUrl;
